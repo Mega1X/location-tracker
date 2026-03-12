7e0aa82b9bca2e5cf008b31a3cb05f3e235f154d
Mega1X <mega31x1@outlook.com>
2026-03-12 11:45:39 -0400

    Setup GitHub actions to inject Firebase API keys securely

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
-// TODO: Replace with your Firebase project configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBjJt" + "qZ517Nm9dO" + "9FPpDosCPi" + "NQlZK1SBM",
-  authDomain: "location-tracker-8145f.firebaseapp.com",
-  databaseURL: "https://location-tracker-8145f-default-rtdb.firebaseio.com",
-  projectId: "location-tracker-8145f",
-  storageBucket: "location-tracker-8145f.firebasestorage.app",
-  messagingSenderId: "800065087000",
-  appId: "1:800065087000:web:db5df9a70a4a5d206fc56d"
-};
+import { firebaseConfig } from './env.js';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
